a904e29115db1486ddf72e990831db4f587f521c
hojong <bcpjh8624@naver.com>
2021-12-06 13:45:57 +0900

    jongho motor

--- LED.c
+++ LED.c
@@ -3,30 +3,15 @@
  #define LED_PIN_1 4
  #define LED_PIN_2 17
  #define LED_PIN_3 18
- #define LED_PIN_4 27
- #define LED_PIN_5 22
- #define LED_PIN_6 23
- #define LED_PIN_7 24
- #define LED_PIN_8 25
- #define LED_PIN_9 6
- #define LED_PIN_10 12
- #define LED_PIN_11 13
- #define LED_PIN_12 16
- #define LED_PIN_13 19
- #define LED_PIN_14 20
- #define LED_PIN_15 26
- #define LED_PIN_16 21
- #define MAX_LED_NUM 16
+ #define MAX_LED_NUM 3
 
-const int LedPinTable[16] {
+const int LedPinTable[3] {
     LED_PIN_1, LED_PIN_2, LED_PIN_3,
-     LED_PIN_4, LED_PIN_5, LED_PIN_6,
-     LED_PIN_7. LED_PIN_8, LED_PIN_9,
-    LED_PIN_10, LED_PIN_11, LED_PIN_12,
-    LED_PIN_13, LED_PIN_14, LED_PIN_15,
-    LED_PIN_16)
+}
 
-int main(void){
+void LEDRun(void);
+
+void LEDRun{
     int i=0;
     if(wiringPiSetupGpio() == -1 )
         return 1;
@@ -34,17 +19,19 @@ int main(void){
     {
         pinMode(LedPinTable[i],OUTPUT);
         digitalWrite(LedPinTable[i],LOW);
-
     }
     i=0;
-    while(1)
+    if(temp<=10)
     {
-        digitalWrite(LedPinTable[i],HIGH);
-        delay(200);
-        digitalWrite(LedPinTable[i],LOW);
-        i++
-
-        if(i==MAX_LED_NUM)i==0;
+        digitalwrite(LedPinTable[0],HIGH)
+    }
+    else if(temp>10 && temp<20);
+    {
+        digitalwrite(LedPinTable[1],HIGH)
+    }
+    else
+    {
+        digitalwrite(LedPinTable[2],HIGH)
     }
     return 0;
 }
